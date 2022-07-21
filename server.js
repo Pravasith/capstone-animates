f1e593302a4e118bf5652f724e208e7eb6959dd2
pravasith <pravasith@gmail.com>
2022-07-20 22:38:13 -0400

    heroku push

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const path = require("path")
 const express = require("express")
 const app = express()
-const publicPath = path.join(__dirname, "build")
-console.log(publicPath)
+
 const port = process.env.PORT || 3000
-app.use(express.static(publicPath))
-app.get("*", (req, res) => {
-    // res.sendFile(path.join(publicPath, "index.html"))
-    res.send("HELLO: " + publicPath)
+
+app.use(express.static(path.join(__dirname, "build")))
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
 })
+
 app.listen(port, () => {
     console.log("Server is up and running @" + port)
 })
