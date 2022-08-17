51288e7ccca20b057281e4eb5d760aabf3e59d54
pravasith <pravasith@gmail.com>
2022-08-17 16:41:10 -0400

    login done

--- src/components/PostNew/index.jsx
+++ src/components/PostNew/index.jsx
@@ -7,18 +7,57 @@ import TextArea from "../UI/TextArea"
 import Button from "../UI/Button"
 import { useContext } from "react"
 import AppContext from "../../contexts/AppContext"
+import { useState } from "react"
 
 const PostNew = props => {
     const { globalState } = useContext(AppContext)
 
+    const [image, setImage] = useState()
+
+    const widget = window.cloudinary.createUploadWidget(
+        {
+            cloudName: "dyvki0hhn",
+            uploadPreset: "etu3pxka",
+        },
+        (error, result) => {
+            if (!error && result && result.event === "success") {
+                setImage(
+                    result.info.secure_url
+                        .split("upload/")
+                        .join("upload/c_limit,h_1000,w_800/")
+                )
+            }
+        }
+    )
+
+    const uploadHandler = () => {
+        widget.open()
+    }
+
     return (
-        <div className="flex-row-north w-full border-b-2 border-black">
-            <Icon src={globalState.user.pp} />
-            <div className="w-full ml-2 flex-col-north ">
-                <TextArea placeholder={"What's on your mind?"} />
-                <div className="w-full flex-row-spaced my-2">
-                    <Button iconSrc={upload} text={"Upload"} />
-                    <Button iconSrc={send} text={"Post"} />
+        <div className="flex-row-col">
+            <div className="flex-row-north w-full border-b-2 border-black">
+                <Icon src={globalState.user.pp} />
+                <div className="w-full ml-2 flex-col-north ">
+                    <TextArea placeholder={"What's on your mind?"} />
+
+                    {image && (
+                        <div>
+                            <img
+                                className="image mt-4 border-4 border-red-400 rounded-2xl"
+                                src={image}
+                            />
+                        </div>
+                    )}
+
+                    <div className="w-full flex-row-spaced my-4">
+                        <Button
+                            onClick={uploadHandler}
+                            iconSrc={upload}
+                            text={"Upload"}
+                        />
+                        <Button iconSrc={send} text={"Post"} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -3,15 +3,20 @@ import Icon from "../Icon"
 const Button = props => {
     return (
         <button
+            onClick={props.onClick}
             type={props.type || "button"}
-            className={`px-2 flex-row-center border-2 border-black rounded-full bg-black hover:border-blue-400 ${props.className}`}
+            className={`px-4 flex-row-center border-4 border-black rounded-full bg-black hover:border-blue-400 active:border-yellow-400 ${props.className}`}
         >
             {props.iconSrc && (
-                <div className="pr-2">
+                <div className="pr-4">
                     <Icon src={props.iconSrc} size={"sm"} noBackground={true} />
                 </div>
             )}
-            <div className={`text-white ${props.iconSrc && "pr-2"}`}>
+            <div
+                className={`text-white font-bold py-2 ${
+                    props.iconSrc && "pr-4"
+                }`}
+            >
                 {props.text}
             </div>
         </button>
